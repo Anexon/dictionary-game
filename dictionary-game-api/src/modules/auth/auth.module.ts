bc7aa6ce8063ff92f049667f213b1c5d6cffeead
Rubén Triviño Juárez <ruben@willimbu.com>
2020-06-27 10:48:45 +0200

    update: extend token duration

--- dictionary-game-api/src/modules/auth/auth.module.ts
+++ dictionary-game-api/src/modules/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '86400s' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
